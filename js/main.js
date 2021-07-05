dcb1a5ac8f8ebd8bf80dd358477de676e9f7977c
Zardasht Rwandzi <35077108+codezardasht@users.noreply.github.com>
2021-07-05 17:57:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "July 20, 2021 00:00:00",
+	date: "July 15, 2021 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
